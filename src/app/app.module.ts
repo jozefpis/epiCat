55edb23c05f335a41d67dfc22bd6209fdf965595
Jozef Pis <info@jozefpis.sk>
2019-07-31 17:25:02 +0200

    remove cookie dependency from appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { VoteComponent } from './vote/vote.component';
 import { CatService } from './services/cat.service';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { CookieService } from 'ngx-cookie-service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TopComponent } from './top/top.component';
 import { StatsComponent } from './stats/stats.component';
@@ -29,8 +28,7 @@ import { StatsComponent } from './stats/stats.component';
     BrowserAnimationsModule
   ],
   providers: [
-    CatService,
-    CookieService
+    CatService
   ],
   bootstrap: [AppComponent]
 })
